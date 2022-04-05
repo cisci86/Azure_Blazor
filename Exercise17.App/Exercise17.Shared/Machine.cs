96272b60ab9740bfdc726a686e969bf313f77afb
Cecilia Sjögren <cisci_13@hotmail.com>
2022-04-05 16:59:52 +0200

    Add project files.

---
+++ Exercise17.App/Exercise17.Shared/Machine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise17.Shared
+{
+    public class Machine
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool Online { get; set; }
+        
+    }
+}
